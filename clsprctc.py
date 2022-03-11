47f4bc5e1d1289716b18ba5a8ced4784b9e24437
idfoxdale <ishantkdas@gmail.com>
2022-03-11 20:36:43 +0530

    list bc

--- clsprctc.py
+++ clsprctc.py
@@ -22,3 +22,22 @@ a.It is used to skip the yield statement of a generator and return a value of No
 b.It is used to pass control from one statement block to another.
 c.It is a null operation used mainly as a placeholder in functions, classes, etc. <----
 d.It is used to skip the rest of a while or for loop and return to the start of the loop. """
+
+mynewliststatic = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+mynewliststatic.append(11)
+print(mynewliststatic)
+mynewliststatic.insert(0, 12)
+print(mynewliststatic)
+mynewliststatic.insert(3, "string")
+print(mynewliststatic)
+
+myemptylist = []
+def myfunction():
+    if len(myemptylist) == 0:
+        print("myemptylist is empty")
+        print("Adding to myemptylist")
+        myemptylist.append(1)
+    else:
+        print("myemptylist is not empty")
+
+myfunction()
