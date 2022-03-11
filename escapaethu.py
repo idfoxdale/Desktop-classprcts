e313d625435626d17c1dbae67c379a525d0ea1d2
idfoxdale <ishantkdas@gmail.com>
2022-03-11 22:14:34 +0530

    str methods

--- escapaethu.py
+++ escapaethu.py
@@ -1,2 +1,7 @@
-# new script starting dicarding all previous due to wrong copy of the script
-
+# new script starting dicarding all previous due to wrong copy of the scrip
+#create a new file with the name of the script
+string1  =   "encapsulation"
+string2  =   "encapsulation2"
+string1.capitalize()
+print(string1.capitalize())
+print(string2.capitalize())
