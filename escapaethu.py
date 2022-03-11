bc3617e926c17b3bf6ca73eff40edb466c4bfc1c
idfoxdale <ishantkdas@gmail.com>
2022-03-11 22:03:27 +0530

    new commigukjt

---
+++ escapaethu.py
@@ -0,0 +1,7 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+my_set = {0, 'apple', 3.5}
+print(my_set)
+{0, 3.5, 'apple'}
+print(type(my_set))
+<class 'set'>
