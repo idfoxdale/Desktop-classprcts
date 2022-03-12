7a6baffbeb02c875bbc723cdb5b7251b6f00923b
idfoxdale <ishantkdas@gmail.com>
2022-03-12 08:58:42 +0530

    new commit 12mar2022

---
+++ defnew.py
@@ -0,0 +1,12 @@
+def print_alpha_nums(abc_list, num_list):
+    for char in abc_list:
+        for num in num_list:
+            print(char, num)
+    return
+
+print_alpha_nums(['a', 'b', 'c'], [1, 2, 3])
+
+list1 = [1, "string", {"key1": "value1", "key2": "value2" }]
+print(list1)
+print(list1[0])
+print(list1.insert(0, 0))
