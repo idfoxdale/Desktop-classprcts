17a35e258f2b131734e886e2bb06578fc039e73d
idfoxdale <ishantkdas@gmail.com>
2022-03-14 08:37:59 +0530

    today new

---
+++ classnewclass.py
@@ -0,0 +1,13 @@
+class Car:
+    def __repr__(self):
+        return "Car()"
+    def __str__(self):
+        return "member of Car"
+
+TataNexon = Car()
+TataNexon.price = 14000
+TataNexon.owner = "Prashant"
+TataNexon.city = "Bengaluru"
+TataNexon.buydate = "April 2022"
+
+print(TataNexon)
