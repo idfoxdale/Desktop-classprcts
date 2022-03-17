7083bb87505b5c0e21a06582aa95ab113a433d6e
idfoxdale <ishantkdas@gmail.com>
2022-03-17 17:08:06 +0530

    putyhon

--- 16march2022/class_iherrit.py
+++ 16march2022/class_iherrit.py
@@ -14,3 +14,42 @@ class Child2(Basee):
 Child1()
 
 Child2()
+
+# Python program to
+# demonstrate init with
+# inheritance
+
+class A(object):
+	def __init__(self, something):
+		print("A init called")
+		self.something = something
+		
+
+class B(A):
+	def __init__(self, something):
+		# Calling init of parent class
+		A.__init__(self, something)
+		print("B init called")
+		self.something = something
+		
+obj = B("Something")
+
+
+# Python program to
+# demonstrate init with
+# inheritance
+
+class A(object):
+	def __init__(self, something):
+		print("A init called")
+		self.something = something
+		
+
+class B(A):
+	def __init__(self, something):
+		print("B init called")
+		self.something = something
+		# Calling init of parent class
+		A.__init__(self, something)
+		
+obj = B("Something")
