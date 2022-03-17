b82433583f935771c66cc4bd6b337ad54f4788a7
idfoxdale <ishantkdas@gmail.com>
2022-03-17 12:28:25 +0530

    commit mid day 17marcg22

---
+++ 16march2022/iview.py
@@ -0,0 +1,34 @@
+from unicodedata import name
+
+
+for i in [1, 2, 3, 4][::-1]:
+    print (i)
+
+# [4]
+# [3]
+# [2]
+# [1]
+
+names = ['Chris', 'Jack', 'John', 'Daman']
+print(names[-1][-3])
+
+# 'Daman', 'John', 'Jack'
+
+type(set(names))
+
+print(names)
+
+class myClass:
+    def __init__(self, attrib1, attrib2):
+        self.attrib1 = attrib1
+        self.attrib2 = attrib2
+
+    def display_message(attrib1, attrib2):
+        print("myClass inst" + attrib1 + attrib2)
+
+
+objectc1 = myClass()
+
+class childClass(myClass):
+    
+
