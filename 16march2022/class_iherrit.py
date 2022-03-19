be08cff500cdbe4f3da8c7e7ff4e30ee514a27e1
idfoxdale <ishantkdas@gmail.com>
2022-03-19 14:20:17 +0530

    test intermediate

--- 16march2022/class_iherrit.py
+++ 16march2022/class_iherrit.py
@@ -1,62 +1,3 @@
-""" class Basee:
-    def __init__(self):
-        print("Base Created") 
-
-class Child1(Basee):
-    def __init__(self):
-        Basee.__init__(self)
-
-class Child2(Basee):
-    def __init__(self):
-        super(Child2, self).__init__()
-
-
-Child1()
-
-Child2()
-
-# Python program to
-# demonstrate init with
-# inheritance
-
-class A(object):
-	def __init__(self, something):
-		print("A init called")
-		self.something = something
-		
-
-class B(A):
-	def __init__(self, something):
-		# Calling init of parent class
-		A.__init__(self, something)
-		print("B init called")
-		self.something = something
-		
-obj = B("Something")
-
-
-# Python program to
-# demonstrate init with
-# inheritance
-
-class A(object):
-	def __init__(self, something):
-		print("A init called")
-		self.something = something
-		
-
-class B(A):
-	def __init__(self, something):
-		print("B init called")
-		self.something = something
-		# Calling init of parent class
-		A.__init__(self, something)
-		
-obj = B("Something")
- """
-
-
-
 class Basee:
     def __init__(self):
         print("Base Created") 
@@ -112,5 +53,3 @@ class B(A):
 		A.__init__(self, something)
 		
 obj = B("Something")
-
-
