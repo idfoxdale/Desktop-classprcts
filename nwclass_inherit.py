d12c75d5e50d6937414da778d7e63076efc2b42b
idfoxdale <ishantkdas@gmail.com>
2022-03-20 12:31:34 +0530

    ram krishna

---
+++ nwclass_inherit.py
@@ -0,0 +1,8 @@
+class Parent1:
+    def __init__(self, param1, param2):
+        self.param1 = param1
+        self.param2 = param2
+        print("Current instance parameters are parameter#1 = " + str(param1) + ", and parameter#2 = " + str(param2))
+
+objty1 = Parent1("Ram", "Ramayan")
+objty2 = Parent1("Krishna", "Geeta")
