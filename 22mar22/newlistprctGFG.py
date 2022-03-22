5773f85428f7d54a4f0dfd783284b7c9eec0585d
idfoxdale <ishantkdas@gmail.com>
2022-03-22 13:37:04 +0530

    new iview 22mar22 HCL 2nd

---
+++ 22mar22/newlistprctGFG.py
@@ -0,0 +1,27 @@
+List = []
+print("Blank list: ")
+print(List)
+
+List.append(20)
+print("List 1st operation ", List)
+
+List.append(30)
+print("List 2nd append operation ", List)
+
+newstring = "newstring"
+List.append(newstring)
+print("List newstring append operation ", List)
+
+Listnew = [["Apple", "Bnana", "1000", 1000], ["Almond", "Figs", 20, "40"]]
+
+print(Listnew)
+print(len(Listnew))
+print("The size of list 'List' is ", (len(List)))
+
+List.insert(0,4)
+print(List)
+newlist = List.copy()
+print( "new list copied ", newlist)
+necpy = newlist.copy()
+necpy.reverse()
+print(necpy)
