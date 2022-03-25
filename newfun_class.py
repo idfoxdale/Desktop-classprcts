cf3e9516bd688409eda3aac28e2d9c5853d4e61c
idfoxdale <ishantkdas@gmail.com>
2022-03-25 09:59:12 +0530

    commit good state

--- newfun_class.py
+++ newfun_class.py
@@ -1,3 +1,6 @@
+from itertools import count
+
+
 class Employee:
     id = 10
     name = "Ajay"
@@ -8,3 +11,36 @@ class Employee:
 
 emp = Employee()
 emp.display()
+
+#deleting the propert of an object
+
+#del emp.id
+#del emp
+
+emp.display()
+
+
+class Student:
+    count = 0
+    def __init__(self):
+        Student.count = Student.count + 1
+
+s1 = Student()
+s2 = Student()
+s3 = Student()
+s4 = Student()
+print("Number of student object: ", Student.count)
+
+
+#python non-parameterised constructor
+
+class Students:
+    #Constructor non-parameterised
+    def __init__(self):
+        print("This is a non parameterized constructor")
+    
+    def show(self, name):
+        print("Hello, ", name)
+
+studentA = Students()
+studentA.show("John Snow")
